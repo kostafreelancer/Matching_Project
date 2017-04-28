1a892206b1b7715533a86717bcef4f5813a3b717
kosta-PC\kosta <nks000000@naver.com>
2017-04-28 18:24:49 +0900

    광식

---
+++ src/lancer/f_mypage/controller/kwangsikController.java
@@ -0,0 +1,36 @@
+package lancer.f_mypage.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("*.f_mypage")
+public class kwangsikController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public kwangsikController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	System.out.println("dd");
+    	
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
